64fb1305c0a1cf7b6b2de35d2e1947f6c4698d02
angelomelo <angelorobsonmelo@gmail.com>
2022-12-15 20:12:42 +0000

    setup paging

--- app/src/main/java/com/example/marvelapp/framework/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/framework/CharacterRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.example.core.data.repository.CharacterRemoteDataSource
 import com.example.core.data.repository.CharacterRepository
 import com.example.core.domain.model.Character
 import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.framework.paging.CharactersPagingSource
 import javax.inject.Inject
 
 class CharacterRepositoryImpl @Inject constructor(
@@ -12,6 +13,6 @@ class CharacterRepositoryImpl @Inject constructor(
 ) : CharacterRepository {
 
     override fun getCharacters(query: String): PagingSource<Int, Character> {
-        return CharactersPaging()
+        return CharactersPagingSource(remoteDataSource, query)
     }
 }
